5d55038ba6b90e3a3542439bbb8d62378f65801d
Kevin <kevin.shehan30@gmail.com>
2024-11-09 13:47:18 +0530

    javascript function addedchoices, computerChoice

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-const choices=['rock','paper','scissors'];
-const playerDisplay=getElementById('playerDisplay');
-const computerDisplay=getElementById('computerDisplay');
+const choices = ['rock', 'paper', 'scissors'];
+const playerDisplay = getElementById('playerDisplay');
+const computerDisplay = getElementById('computerDisplay');
+const resultDisplay = getElementById('resultDisplay');
 
 
+function playGame(playChoices) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    console.log(computerChoice);
+    let result = "";
+    if (playChoices === computerChoice) {
+        result = "IT'S A TIE"
+    }
+    else {
+        switch (playChoices) {
+            case 'rock':
+                result = (computerChoice === 'paper')? 'Computer wins!' : 'Player wins!';
+                break;
+            case 'paper':
+                result = (computerChoice === 'scissors')? 'Computer wins!' : 'Player wins!';
+                break;
+            case'scissors':
+                result = (computerChoice === 'rock')? 'Computer wins!' : 'Player wins!';
+                break;
+         }
+    }
+    playerDisplay.textContent = "Player chose: " + playChoices;
+    computerDisplay.textContent = "Computer chose: " + computerChoice;
+    resultDisplay.textContent = result;
+}
+
+playGame(choices)
